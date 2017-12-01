55d51b7c3dbdcdf00299f62db2853e5bd78ddcda
RyleeAndrews <ryleeandrews73@gmail.com>
2017-12-01 13:38:46 -0800

    added code. Nothing is runnin working on errors

---
+++ src/main.js
@@ -0,0 +1,35 @@
+const faker = require('faker');
+import React from 'react';
+import ReactDom from 'react-dom'
+
+import '../src/style/main.scss';
+
+class App extends React.component {
+
+  constructor(props){
+    super(props);
+
+    this.generateRandomIpsum = this.generateRandomIpsum.bind(this);
+
+    this.state = {
+      content: faker.word(),
+    };
+  }
+
+  generateRandomIpsum(){
+    let content = faker.word();
+    this.setState({content});
+  }
+  render(){
+    return (
+      <div>
+        <header>
+          <div className="content">{this.state.content}</div>
+          <h1> Generate Cowsay Lorem </h1>
+          <button onClick={this.generateRandomIpsum}></button>
+        </header>
+      </div>
+    );
+  }
+}
+ReactDom.render(<App/>, document.getElementById('root'));
